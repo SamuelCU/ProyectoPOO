f7f0bf8832bb1c4bdeab0209ebfb37536548a427
Samuel Cuti <samcuti.03@gmail.com>
2024-01-26 10:39:20 -0500

    v3.0

--- src/main/java/com/mycompany/exportarexcel/views/login.java
+++ src/main/java/com/mycompany/exportarexcel/views/login.java
@@ -3,6 +3,7 @@ package com.mycompany.exportarexcel.views;
 import com.mycompany.exportarexcel.principal;
 
 import javax.swing.*;
+import java.awt.*;
 import java.security.Principal;
 
 public class login extends javax.swing.JFrame {
@@ -36,16 +37,64 @@ public class login extends javax.swing.JFrame {
 
     private void realizarInicioSesion() {
         // Lógica para verificar el inicio de sesión
-        JPanel panel = new JPanel();
-        JLabel userLabel = new JLabel("Usuario:");
-        JTextField usuarioField = new JTextField(10);
-        JLabel passwordLabel = new JLabel("Contraseña:");
-        JPasswordField contrasenaField = new JPasswordField(10);
 
-        panel.add(userLabel);
-        panel.add(usuarioField);
-        panel.add(passwordLabel);
-        panel.add(contrasenaField);
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+
+        JLabel titleLabel = new JLabel("LOG IN");
+        //JLabel userLabel = new JLabel("Usuario:");
+        //JTextField usuarioField = new JTextField(10);
+        //JLabel passwordLabel = new JLabel("Contraseña:");
+        //JPasswordField contrasenaField = new JPasswordField(10);
+
+        Font fontTitle = new Font("Centhuric Gotic", Font.PLAIN, 20);
+        Font fontlabel = new Font("Centhuric Gotic", Font.PLAIN, 15);
+
+        titleLabel.setFont(fontTitle);
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+        panel.add(titleLabel, gbc);
+        //panel.add(userLabel);
+        //panel.add(usuarioField);
+        //panel.add(passwordLabel);
+        //panel.add(contrasenaField);
+
+        gbc.gridwidth = 1;
+        gbc.gridy = 1;
+
+        JLabel userLabel = new JLabel("Usuario:");
+        gbc.gridx = 0;
+        panel.add(userLabel, gbc);
+        userLabel.setFont(fontlabel);
+
+        JTextField usuarioField = new JTextField(10);
+        gbc.gridx = 1;
+        panel.add(usuarioField, gbc);
+        usuarioField.setFont(fontlabel);
+
+        gbc.gridy = 2;
+
+        JLabel passwordLabel = new JLabel("Contraseña:");
+        gbc.gridx = 0;
+        panel.add(passwordLabel, gbc);
+        passwordLabel.setFont(fontlabel);
+
+        JPasswordField contrasenaField = new JPasswordField(10);
+        gbc.gridx = 1;
+        panel.add(contrasenaField, gbc);
+        contrasenaField.setFont(fontlabel);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+
 
         int result = JOptionPane.showConfirmDialog(this, panel, "Inicio de Sesión",
                 JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
